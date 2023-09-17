3c875c86acc3a02f631e280a6bc822c0ba905b02
sssuearth <128416382+sssuearth@users.noreply.github.com>
2023-09-17 14:28:00 +0900

    Using p5Util

--- src/w2/sketches/sketch6/script/sketch.js
+++ src/w2/sketches/sketch6/script/sketch.js
@@ -1,5 +1,11 @@
 function setup() {
-  setCanvasContainer();
+  setCanvasContainer('canvas-goes-here', 400, 300, true);
+  //true 400*300 비율을 유지하며 플렉시블하게 조절됨
+  background('#ff7733');
 }
 
-function draw() {}
+function draw() {
+  background('#ff7733');
+  circle(mouseX, mouseY, width * 0.1);
+  //width * 0.1 비례에 따라 원크기 자동 절
+}
