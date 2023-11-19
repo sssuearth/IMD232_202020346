38b14668913a3f7af94e8a736aea8572e353f011
sssuearth <128416382+sssuearth@users.noreply.github.com>
2023-11-20 04:28:00 +0900

    w111000

--- src/w10/sketches/sketch1/script/sketch.js
+++ src/w10/sketches/sketch1/script/sketch.js
@@ -1,57 +1,99 @@
-// let = var
+let cells = [];
 
-// let Engine = Matter.Engine,
-//   Render = Matter.Render,
-//   Runner = Matter.Runner,
-//   Bodies = Matter.Bodies,
-//   Composite = Matter.Composite;
+const colNum = 50,
+  rowNum = colNum;
 
-// matter.js 렌더를 사용하려면 필수
-let Engine = Matter.Engine; // 일어나는 일들 계산, 안에 월드
-let Render = Matter.Render; // 그림그려주는
-let Runner = Matter.Runner; // draw의 역할
-let Bodies = Matter.Bodies; // 다각형 등의 물체를 만들 수 있음
-let Composite = Matter.Composite; // 추가, 해당하는 물체를 넣어주는
+let w, h;
 
-// 필수과정 1: 엔진 만들기
-// Engine = Matter.Engine
-let engine = Engine.create();
+let patternBlock = {
+  width: 4,
+  height: 4,
+  trueBlockIdx: [5, 6, 9, 10],
+};
 
-// 캔버스 : 컨테이너 내 생성
-let elem = document.querySelector('#canvas');
-console.log(elem);
+let patternBlinker = {
+  width: 5,
+  height: 5,
+  trueBlockIdx: [11, 12, 13],
+};
 
-// 필수과정 2: 렌더러 만들기
-let render = Render.create({
-  element: elem,
-  engine: engine,
-  options: {
-    wireframeBackground: '#29155c',
-    width: elem.clientWidth,
-    height: (elem.clientWidth / 4) * 3,
-  },
-});
-console.log(render);
+let patternToad = {
+  width: 6,
+  height: 6,
+  trueBlockIdx: [14, 15, 16, 19, 20, 21],
+};
 
-// 옵션과정 1: 물체 만들기
-// 두개의 박스와 그라운드
-let boxA = Bodies.rectangle(400, 200, 80, 80);
-let boxB = Bodies.rectangle(450, 50, 80, 80);
-let ground = Bodies.rectangle(400, 610, 400, 20, { isStatic: true });
-console.log(ground);
+let patternGlider = {
+  width: 5,
+  height: 5,
+  trueBlockIdx: [7, 13, 16, 17, 18],
+};
 
-// 옵션과정 2: 물체를 세계에 추가하기
-// 어레이로 괄호(아래)
-// Composite.add(engine.world, [boxA, boxB, ground]);
-Composite.add(engine.world, boxA);
-Composite.add(engine.world, boxB);
-Composite.add(engine.world, ground);
+// 패턴들을 사용해 중간에 바꿀때
 
-// 필수과정 3: 그림그리기
-Render.run(render);
+//패턴이 들어갈 위치
+let addPattern = (x, y, pattern) => {
+  for (let row = 0; row < pattern.height; row++) {
+    for (let col = 0; col < pattern.width; col++) {
+      const colIdx = x + col;
+      const rowIdx = y + row;
+      const idx = colNum * rowIdx + colIdx;
+      // idx가 패턴의 idx에 있는 숫자와 동일하다면
+      if (pattern.trueBlockIdx.includes(pattern.width * row + col)) {
+        cells[idx].state = true;
+      }
+    }
+  }
+};
 
-// 필수과정 4: 자동으로 계속 동작하게 해주는 장치 만들기
-let runner = Runner.create();
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
 
-// 필수과정 5: 자동 뺑뺑이에게 엔진을 등록해서 ㄱㄱㄱ
-Runner.run(runner, engine);
+  randomSeed(1);
+
+  w = width / colNum;
+  h = height / rowNum;
+
+  for (let row = 0; row < rowNum; row++) {
+    for (let col = 0; col < colNum; col++) {
+      const x = w * col;
+      const y = h * row;
+      // const state = random() < 0.5;
+      const state = false;
+      const idx = colNum * row + col;
+      const newCell = new Cell(x, y, w, h, state, idx);
+      cells.push(newCell);
+    }
+  }
+
+  addPattern(0, 0, patternBlock);
+  addPattern(20, 10, patternBlock);
+  addPattern(30, 10, patternBlinker);
+  addPattern(10, 30, patternToad);
+  addPattern(1, 10, patternGlider);
+
+  cells.forEach((eachCell) => {
+    eachCell.addFriends(cells);
+  });
+
+  console.log(cells);
+
+  frameRate(4);
+  background('white');
+}
+
+function draw() {
+  background('white');
+
+  cells.forEach((eachCell) => {
+    eachCell.calcNextState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.updateState();
+  });
+
+  cells.forEach((eachCell) => {
+    eachCell.display();
+  });
+}
