3407873ae0ccc9364e2ef60c53dc255a8f55fabb
sssuearth <128416382+sssuearth@users.noreply.github.com>
2023-12-17 15:43:57 +0900

    542

---
+++ src/w14/sketches/sketch542/script/sketch.js
@@ -0,0 +1,108 @@
+// 매터 쓰기 위한 기본 변수
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Composites = Matter.Composites,
+  Common = Matter.Common,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Composite = Matter.Composite,
+  Bodies = Matter.Bodies;
+
+// create engine
+var engine = Engine.create(),
+  world = engine.world;
+
+function setup() {
+  setCanvasContainer('canvas', 800, 600, true);
+  background('white');
+}
+
+function draw() {
+  background('white');
+}
+
+// create renderer
+const elem = document.querySelector('#canvas');
+var render = Render.create({
+  element: elem,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    showAngleIndicator: false,
+    wireframes: false,
+  },
+});
+
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+Runner.run(runner, engine);
+
+// add bodies
+var offset = 10,
+  options = {
+    isStatic: true,
+  };
+
+world.bodies = [];
+
+// these static walls will not be rendered in this sprites example, see options
+Composite.add(world, [
+  Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, options),
+  Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, options),
+  Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, options),
+  Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, options),
+]);
+
+var stack = Composites.stack(20, 20, 10, 4, 0, 0, function (x, y) {
+  if (Common.random() > 0.35) {
+    return Bodies.rectangle(x, y, 64, 64, {
+      render: {
+        strokeStyle: '#ffffff',
+        sprite: {
+          texture: './img/box.png',
+        },
+      },
+    });
+  } else {
+    return Bodies.circle(x, y, 46, {
+      density: 0.0005,
+      frictionAir: 0.06,
+      restitution: 0.3,
+      friction: 0.01,
+      render: {
+        sprite: {
+          texture: './img/ball.png',
+        },
+      },
+    });
+  }
+});
+
+Composite.add(world, stack);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
+
+// fit the render viewport to the scene
+Render.lookAt(render, {
+  min: { x: 0, y: 0 },
+  max: { x: 800, y: 600 },
+});
